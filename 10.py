567f535cd2fb950bfa7c8d58fbe5653a27547628
zxc_ghoul <AlexisV.Shishkin@gmail.com>
2023-11-12 12:40:59 +0700

    task 10 solution
    task 10 solution

---
+++ 10.py
@@ -0,0 +1,38 @@
+class LRUCache:
+    def __init__(self, capacity=16):
+        self.capacity = capacity
+        self.cache = {}
+        self.order = []
+
+    def get(self, key):
+        if key in self.cache:
+            self.order.remove(key)
+            self.order.append(key)
+            return self.cache[key]
+        else:
+            return None
+
+    def put(self, key, value):
+        if key in self.cache:
+            self.cache[key] = value
+            self.order.remove(key)
+            self.order.append(key)
+        else:
+            if len(self.cache) >= self.capacity:
+                oldest_key = self.order.pop(0)
+                del self.cache[oldest_key]
+
+            self.cache[key] = value
+            self.order.append(key)
+
+
+cache = LRUCache(2)
+cache.put(1, 1)
+cache.put(2, 2)
+print(cache.get(1))
+cache.put(3, 3)
+print(cache.get(2))
+cache.put(4, 4)
+print(cache.get(1))
+print(cache.get(3))
+print(cache.get(4))
